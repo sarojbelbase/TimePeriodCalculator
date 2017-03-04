2a49de2db37f98e19d7360b5636a5db65cce40d2
Siddharth Belbase <siddharthbelbase@gmail.com>
2017-03-04 15:37:51 +0545

    Update main.c

--- main.c
+++ main.c
@@ -2,49 +2,90 @@
 #include <stdlib.h>
 int main(int argc, char const *argv[])
 {
-	int AStart,BStart,AEnd,BEnd,Got1,Got2,Got3,Got,ANew,BNew,AResult,BResult,CResult,Result,AFix;
+	int AStart,BStart,AEnd,BEnd,Got1,Got2,Got3,Got,ANew,BNew,AResult,BResult,CResult,Result,AFix,AFix2;
 	printf("What's the Starting Time ?\n");
 	scanf("%d%d",&AStart,&BStart);
 	printf("When it's gonna end, then ?\n");
 	scanf("%d%d",&AEnd,&BEnd);
-	if (AStart<12)
-	{AStart=AStart+12;}
-	
-	else if (AStart==12)
-	{AStart=AStart;}
-	
-	if (AEnd<12)
-	{AEnd=AEnd+12;}
-	
-	else if (AEnd==12)
-	{AEnd=AEnd;}
+   
+   while(AStart==AEnd)
+		{
+		AResult=0;
+		BResult=0;
+		CResult=BEnd-BNew;
+		AEnd=AEnd-12;
+		AStart=AStart-12;
+		}	
 
-	if (AStart>AEnd)
+   if (AStart<AEnd)
+	
+	while (AEnd<=12 || AStart<=12 )
+		{
+			AEnd=AEnd+12;
+			AStart=AStart+12;
+			AFix=AEnd-AStart-1;
+			Got1=60-BStart;
+			Got2=BEnd; 
+			Got3=AFix*60;
+		}
+
+   else if(AStart>AEnd) 
 	{
-		AFix=AStart-AEnd;
-	}
-	else if(AStart<AEnd) {
-		AFix=AEnd-AStart;
+	
+		while (AEnd<=12)
+		{
+			AEnd=AEnd+12;
+			AFix=AEnd-AStart-1;
+			Got1=60-BStart;
+			Got2=BEnd; 
+			Got3=AFix*60;
+		}
+	
+		
 	}
 	
-	Got1=60-BStart; //Subtracted 60 from the begginning time
-	Got2=BEnd;  //Since we dont need to add or subtract so its that value
-	Got3=(AFix-1)*60; //if anything remains in the way
 	Got=Got1+Got2+Got3;
-
 	printf("Anyways, Tell the time now ?\n");
 	scanf("%d%d",&ANew,&BNew);
-	if (ANew<12)
-	{ANew=ANew+12;}
-	
-	else if (ANew==12)
-	{ANew=ANew;}
-	
-	AResult=60-BNew;
-	BResult=BEnd;
-	CResult=(AEnd-ANew-1)*60;
-	Result=AResult+BResult+CResult;
-	printf("Actually, Sir Will Teach for only %d Minutes.\n",Got);
-	printf("\t\t\t\t\t You better wait for %d Minutes more. \n", Result);
+
+	if (ANew==AEnd)
+		{
+		AResult=0;
+		BResult=0;
+		CResult=BEnd-BNew;
+		}                                                     //Alright Now >>>Solved
+
+	else if (ANew>AEnd)
+		
+               {
+               	
+		AResult=60-BNew;
+		BResult=BEnd;
+		CResult=(ANew-AEnd-1)*60;
+		
+	}                                               //Alright Now >>>Not Solved
+
+	else if(ANew<AEnd)
+	{
+		if (ANew<12)
+			{
+				ANew=ANew+12;
+				AResult=60-BNew;
+				BResult=BEnd;
+				CResult=(AEnd-ANew-1)*60;
+			}
+		else if (ANew<=12) 
+			{
+				AResult=60-BNew;
+				BResult=BEnd;
+				CResult=(AEnd-ANew-1)*60;
+			}
+	}						//Fixed
+
+	Result=(AResult+BResult+CResult);
+	printf("\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",AStart,AEnd,BEnd,ANew,BNew,AResult,BResult,CResult);
+
+	//printf("Actually, Sir Will Teach for only %d Minutes.\n",Got);
+	//printf("\t\t\t\t\t You better wait for %d Minutes more. \n", Result);
 	return 0;
 }
