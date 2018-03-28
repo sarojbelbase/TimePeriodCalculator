9497353709a5dc119e2ba45e171382e926e8b20f
ThatsSid <siddharthbelbase@gmail.com>
2018-03-28 21:01:16 +0545

    Used Class & Constructors
    + Added more explaining comments
    + Added copy constructors
    + Added more functions
    + Created space between "put to" sign and "get from" sign
    + Mad main() function clutter free!
    + Increased lines of code ~ size

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ class DurationCalculator
   private:  
                 int StartingHours,StartingMinute,StartingSecond,EndingHours,EndingMinute,EndingSecond;
                 int TotalStartingSecs,TotalEndingSecs,VeryTotalSecs;
-                int AmountOfMinutes, ExactSeconds, ExactHours, ExactMinutes;
+                int AmountOfMinutes,ExactSeconds,ExactHours,ExactMinutes;
                 float PercentOfMonth,PercentOfDay;
-                string StartingTimeHalf, EndingTimeHalf;
+                string StartingTimeHalf,EndingTimeHalf;
   public:   
                 void BothSameHalves();
                 void OppositeHalves();
@@ -17,25 +17,27 @@ class DurationCalculator
                 void ConverterAndStuff();
                 void PrintingStuffs();
 };
+
 int main(int argc, char const *argv[])
 {
-DurationCalculator calc; 
 // setting classes
+DurationCalculator calc; 
+//Asks Starting Time & Ending Time
 calc.AskQuestions();
-//Algorithmforbothonamorpm
+//Algorithms that detects AM & PM
 calc.NotActualAlgorithm();
 //Converting to Minutes & Seconds
 calc.ConverterAndStuff();
-// Prints Output
+// Prints Outputs & Calculations
 calc.PrintingStuffs();
 return 0;
 }
 void DurationCalculator :: AskQuestions()
                 {
-                    cout << "What's the Starting Time ? ( Example: 12: 40: 36 PM)"<<endl;
-                    cin >>StartingHours>>StartingMinute>>StartingSecond>>StartingTimeHalf;
-                    cout << "When it's gonna end, then ? ( Example: 5: 20: 36 AM)"<<endl;  
-                    cin >>EndingHours>>EndingMinute>>EndingSecond>>EndingTimeHalf;
+                    cout << "What's the Starting Time ? ( Example: 12: 40: 36 PM)" << endl;
+                    cin >> StartingHours >> StartingMinute >> StartingSecond >> StartingTimeHalf;
+                    cout << "When it's gonna end, then ? ( Example: 5: 20: 36 AM)" << endl;  
+                    cin >> EndingHours >> EndingMinute >> EndingSecond >> EndingTimeHalf;
                      //Asked about the first CalculatedHours time, minute time & remaining CalculatedHours time. 
                 }
 void DurationCalculator :: NotActualAlgorithm()
@@ -69,17 +71,17 @@ void DurationCalculator :: ConverterAndStuff()
 }
 void DurationCalculator :: PrintingStuffs()
 {
-   cout<<endl<<endl;
-   cout<<"The duration between "<<StartingHours<<":"<<StartingMinute<<":"<<StartingSecond<<StartingTimeHalf<<" & ";
-   cout<<EndingHours<<":"<<EndingMinute<<":"<<EndingSecond<<EndingTimeHalf<<" is ";
-   cout<<ExactHours<<"hours, "<<ExactMinutes<<" minutes and "<<ExactSeconds<<" seconds."<<endl;
-   cout<<"\n++++++++++++++++++++++++++++"<<endl;
-   cout<<"Alternate Time Units:\n"<<endl;
-   cout<<VeryTotalSecs<<" Seconds"<<endl;
-   cout<<AmountOfMinutes<<" Minutes"<<endl;
-   cout<<ExactHours<<" Hours"<<endl;
-   cout<<fixed<<setprecision(2)<<PercentOfDay<<"% "<<"of a 24 hour day."<<endl;
-   cout<<PercentOfMonth<<"% "<<"of a month."<<endl;
+   cout << endl << endl;
+   cout << "The duration between "<< StartingHours << ":" << StartingMinute << ":" << StartingSecond << StartingTimeHalf << " & ";
+   cout << EndingHours << ":" << EndingMinute << ":" << EndingSecond << EndingTimeHalf << " is ";
+   cout << ExactHours << "hours, " << ExactMinutes << " minutes and " << ExactSeconds << " seconds." << endl;
+   cout << "\n++++++++++++++++++++++++++++"<<endl;
+   cout << "Alternate Time Units:\n" << endl;
+   cout << VeryTotalSecs << " Seconds" << endl;
+   cout << AmountOfMinutes << " Minutes" << endl;
+   cout << ExactHours << " Hours" << endl;
+   cout << fixed << setprecision(2) << PercentOfDay << "% " << "of a 24 hour day." << endl;
+   cout << PercentOfMonth << "% " << "of a month." << endl;
 }
 void DurationCalculator :: BothSameHalves()
 {
